3af23f607343dcec7f9cafee6b9587440d7b07e9
yangjunping <yangjunping@115.com>
2019-01-31 16:15:15 +0800

    项目文件同步

---
+++ websync/README.md
@@ -0,0 +1,11 @@
+# 项目文件同步
+
+#### 安装
+
+- `cp config.php.example config.php`
+- `ln -s /path/to/websync/websync.php /usr/local/bin/websync` *注意权限实体文件有无执行权限*
+
+#### 用法
+
+- `cd /path/to/project`
+- `websync`
--- websync/websync.php
+++ websync/websync.php
@@ -6,11 +6,20 @@
  * 项目文件同步
  */
 
-$opt = getopt('', ['src::', 'dst::', 'force::']);
+$opt = getopt('', ['src::', 'dst::', 'force::', 'help::']);
+
+$help = isset($opt['help']) ?: false;
+if ($help) {
+    echo <<<DOC
+    websync [--src=本地源路径] [--dst=远程目的路径] [--force 没有 .gitignore 文件时强制同步]
+DOC;
+    echo PHP_EOL;
+    exit;
+}
 
 $force = isset($opt['force']) ?: false;
 if (!file_exists('.gitignore') && !$force) {
-    echo '没有 .gitignore 文件，请确定是否在项目根目录' . PHP_EOL;
+    echo '没有 .gitignore 文件，请确定是否在项目根目录，强制同步可使用参数 --force' . PHP_EOL;
     exit;
 }
 
