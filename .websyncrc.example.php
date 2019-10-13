e6fbd0baf63849580bbfd00d1b612e3238efcf5d
muzk6 <muzk6x@gmail.com>
2019-10-14 01:06:49 +0800

    v2, 接下来交互初始化配置

---
+++ .websyncrc.example.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    // rsync --include=, 不排除的列表
+    'include' => [],
+    // rsync --exclude=, 排除列表
+    'exclude' => [
+        '.DS_Store',
+        '.git/',
+        '.idea/',
+    ],
+    // 远程服务器
+    'remotes' => [
+        // 自定义服务器名称
+        'hostname1' => [
+            '@' => 'websync@host',
+            // 远程目的根目录
+            'path' => '/path/to/',
+            // rsync --chown=, 对所有同步的文件进行 chown, 不设置此项或值为空就不修改文件权限
+            'chown' => 'websync:websync',
+            // rsync -e, 例如指定 ssh 端口
+            'command' => 'ssh -p 22',
+        ],
+    ],
+    // 远程同步到本地
+    'pull' => 'hostname1',
+    // 同步到远程
+    'push' => [
+        'hostname1',
+    ],
+];
--- websync.php
+++ websync.php
@@ -8,12 +8,21 @@
 
 $opt = getopt('h::v::t::', ['remote::',
     'force::', 'test::', 'init::', 'help::',
-]);
+], $ind);
 
-if (isset($opt['h']) || isset($opt['help'])) {
+$isHelp = isset($opt['h']) || isset($opt['help']);
+$isInit = isset($opt['init']);
+$isTest = isset($opt['t']) || isset($opt['test']);
+$action = $argv[$ind] ?? 'push';
+$hostname = $argv[$ind + 1] ?? null;
+
+if ($isHelp) {
     echo <<<DOC
 USAGE
-    websync [OPTION...]
+    websync [OPTION...] [push/pull]
+ARGV
+    push 本地推上远程(默认)
+    pull 远程拉入本地
 OPTION
     -t
     --test
@@ -38,10 +47,10 @@ DOC;
 }
 
 // 配置文件路径
-$pathConf = getenv('HOME') . '/.websyncrc.php';
+$pathConf = './.websyncrc.php';
 
 // 初始化配置
-if (isset($opt['init'])) {
+if ($isInit) {
     if (file_exists($pathConf)) {
         echo "{$pathConf} 已经存在" . PHP_EOL;
         exit;
@@ -50,7 +59,7 @@ if (isset($opt['init'])) {
     system(sprintf('cp %s %s', __DIR__ . '/.websyncrc.example.php', $pathConf));
 
     echo '配置文件初始化完成，请继续编辑配置:' . PHP_EOL;
-    echo 'vim ~/.websyncrc.php' . PHP_EOL;
+    echo 'vim .websyncrc.php' . PHP_EOL;
     exit;
 }
 
@@ -62,117 +71,80 @@ if (!file_exists($pathConf)) {
 }
 $conf = include($pathConf);
 
-// 显示远程配置
-if (isset($opt['remote']) && $opt['remote'] === false) {
-    if (empty($conf['remote'])) {
-        echo '远程配置不存在' . PHP_EOL;
-        exit;
-    }
-
-    print_r($conf['remote']);
-    exit;
-}
-
-$pwd = getcwd();
-
-// 检查项目配置 projects
-$projectName = basename($pwd);
-if (empty($opt['remote']) && !isset($conf['projects'][$projectName])) {
-    echo "不存在项目配置 {$projectName}" . PHP_EOL;
-    echo "vim ~/.websyncrc.php 配置 projects.{$projectName}" . PHP_EOL;
-    exit;
-}
-$projectConf = $conf['projects'][$projectName] ?? [];
-
-// 检查有无为项目指定 remote
-$remote = !empty($opt['remote'])
-    ? (is_array($opt['remote']) ? $opt['remote'] : [$opt['remote']])
-    : ($projectConf['remote'] ?? '');
-if (empty($remote)) {
-    echo '没有指定 remote' . PHP_EOL;
-    echo '使用 --remote=REMOTE 指定' . PHP_EOL;
-    echo "或 vim ~/.websyncrc.php 配置 projects.{$projectName}.remote" . PHP_EOL;
-    exit;
-}
-$remote = array_unique($remote);
-
-// 检查 remote 是否存在
-$remoteConf = $conf['remote'];
-foreach ($remote as $curRemoteName) {
-    if (!isset($remoteConf[$curRemoteName])) {
-        echo "远程配置 {$curRemoteName} 不存在" . PHP_EOL;
-        echo 'vim ~/.websyncrc.php 配置 remote' . PHP_EOL;
-        exit;
-    }
-}
-
-// 配置文件里的 全局-非排除 列表
-$include = [];
-if (!empty($conf['global']['include'])) {
-    $include = array_merge($include, $conf['global']['include']);
-}
-
-// 配置文件里的 项目-非排除 列表
-if (!empty($projectConf['include'])) {
-    $include = array_merge($include, $projectConf['include']);
-}
-
 // 构造用于非排除文件的参数
 $includeParam = [];
-foreach ($include as $v) {
+foreach ($conf['include'] as $v) {
     $v = trim($v);
     $includeParam[] = "--include={$v}";
 }
 $includeParam = implode(' ', $includeParam);
 
-// 配置文件里的 全局-排除 列表
-$ignores = [];
-if (!empty($conf['global']['exclude'])) {
-    $ignores = array_merge($ignores, $conf['global']['exclude']);
-}
-
-// 配置文件里的 项目-排除 列表
-if (!empty($projectConf['exclude'])) {
-    $ignores = array_merge($ignores, $projectConf['exclude']);
-}
-
 // 构造用于排除文件的参数
 $excludeParam = [];
-foreach ($ignores as $ignore) {
+foreach ($conf['exclude'] as $ignore) {
     $ignore = trim($ignore);
     $excludeParam[] = "--exclude={$ignore}";
 }
 $excludeParam = implode(' ', $excludeParam);
 
-foreach ($remote as $curRemoteName) {
-    // 当前使用的远程配置
-    $curRemoteConf = $remoteConf[$curRemoteName];
-
-    $src = $pwd . '/';
-
-    $curProjectName = $projectName;
-    if (!empty($projectConf['alias'])) {
-        if (is_string($projectConf['alias'])) {
-            $curProjectName = $projectConf['alias'];
-        } elseif (isset($projectConf['alias'][$curRemoteName])) {
-            $curProjectName = $projectConf['alias'][$curRemoteName];
-        }
-    }
-    $dst = $curRemoteConf['dst'] . '/' . $curProjectName;
-
-    // 重置文件所属
-    $chown = '--chown=root:root';
-    if (!empty($curRemoteConf['chown'])) {
-        $chown = "--chown={$curRemoteConf['chown']}";
+// 执行 rsync 命令
+$execRsync = function ($hostname, callable $actionHook) use ($conf, $isTest, $includeParam, $excludeParam) {
+    // 检查 remote 是否存在
+    $curRemoteConf = &$conf['remotes'][$hostname];
+    if (!isset($curRemoteConf)) {
+        echo "远程配置 {$hostname} 不存在" . PHP_EOL;
+        echo 'vim .websyncrc.php 配置 remotes' . PHP_EOL;
+        exit;
     }
 
-    $cmd = "rsync -avz --delete --progress {$chown} {$includeParam} {$excludeParam} {$src} {$curRemoteConf['hostname']}:{$dst}";
+    $localPath = './';
+    $remotePath = "{$curRemoteConf['@']}:{$curRemoteConf['path']}";
+    list($chown, $src, $dst) = $actionHook($localPath, $remotePath);
 
-    $isTest = isset($opt['t']) || isset($opt['test']);
+    $command = '';
+    if (!empty($curRemoteConf['command'])) {
+        $command = "-e \"{$curRemoteConf['command']}\"";
+    }
+
+    $cmd = "rsync -avz --delete --progress {$chown} {$command} {$includeParam} {$excludeParam} {$src} {$dst}";
+
     if ($isTest) {
         $cmd = preg_replace('/^rsync/', 'rsync -n', $cmd);
         echo $cmd . PHP_EOL;
     }
 
     system($cmd);
+};
+
+// 执行 action
+switch ($action) {
+    case 'push':
+        if ($hostname) {
+            $hostnameList = explode(',', $hostname);
+        } else {
+            $hostnameList = $conf['push'];
+        }
+
+        foreach ($hostnameList as $hostname) {
+            $hostname = trim($hostname);
+            $execRsync($hostname, function ($localPath, $remotePath) use ($hostname) {
+                // 重置文件归属
+                $chown = '--chown=websync:websync';
+                if (!empty($conf['remotes'][$hostname]['chown'])) {
+                    $chown = "--chown={$conf['remotes'][$hostname]['chown']}";
+                }
+
+                return [$chown, $localPath, $remotePath];
+            });
+        }
+        break;
+    case 'pull':
+        $execRsync($hostname ? trim($hostname) : $conf['pull'], function ($localPath, $remotePath) {
+            return ['', $remotePath, $localPath];
+        });
+        break;
+    default:
+        echo '不存在的 action, websync -h 查看帮助' . PHP_EOL;
+        exit;
+        break;
 }
