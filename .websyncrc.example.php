a77007eff19177b68c1600a1a9540d8b48bbccea
muzk6 <muzk6x@gmail.com>
2019-10-17 13:06:06 +0800

    1. 查看配置文件说明 2. 版本判断逻辑调整 3. 初始化配置文件的机制由 文本替换 改为 数组重置

--- .websyncrc.example.php
+++ .websyncrc.example.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    // 要求 websync 的版本号
-    'version' => ['2.1', '>='],
+    // 配置文件的版本号
+    'version' => '2.2',
     // rsync --include=, 不排除的列表
     'include' => [],
     // rsync --exclude=, 排除列表
@@ -20,14 +20,14 @@ return [
             // rsync -e, 例如指定 ssh 端口
             'command' => 'ssh -p 22',
             // 远程根目录
-            'path' => '/path/to/',
+            'path' => '/path/to',
             // rsync --chown=, 对所有同步的文件进行 chown, 不设置此项或值为空就不修改文件权限
             'chown' => 'websync:websync',
         ],
     ],
     // 远程同步到本地
     'pull' => 'hostname1',
-    // 同步到远程
+    // 本地同步到远程
     'push' => [
         'hostname1',
     ],
--- websync.php
+++ websync.php
@@ -5,7 +5,7 @@
  * 基于 rsync 的本地、远程 双向同步工具
  */
 
-const VERSION = '2.1';
+const VERSION = '2.2';
 
 if (version_compare(phpversion(), '7.1', '<')) {
     echo 'PHP版本必须 >=7.1' . PHP_EOL;
@@ -21,34 +21,40 @@ $isGlobal = isset($opt['g']) || isset($opt['global']);
 $action = $argv[$ind] ?? 'push';
 $hostname = $argv[$ind + 1] ?? null;
 
+// 查看帮助
 if ($isHelp) {
-    echo <<<DOC
+    if ($action == 'config') {
+        echo file_get_contents(__DIR__ . '/.websyncrc.example.php') . PHP_EOL;
+    } else {
+        echo <<<DOC
 基于 rsync 的本地、远程 双向同步工具
 USAGE
     websync [OPTION...] [push/pull] [hostname1]
-ARGV
-    push [hostname1[,hostname2]] 本地推上远程(默认动作)，没有指定 hostname 时读取配置
-    pull [hostname1] 远程拉入本地，没有指定 hostname 时读取配置
+ACTION
+    push [hostname1[,hostname2]] 本地同步到远程(默认动作)，没有指定 hostname 时读取配置
+    pull [hostname1] 远程同步到本地，没有指定 hostname 时读取配置
 OPTION
-    --init
-        初始化配置文件
-    -t
-    --test
-        测试模式，只打印而不执行同步命令
-    -g
-    --global
+    --init 初始化配置文件
+    -t, --test 测试模式，只打印而不执行同步命令
+    -g, --global config/remote 全局配置
+        config 查看全局配置文件的路径
         remote 新增/编辑 全局远程服务器
-    -h
-    --help
-        帮助
+    -h, --help [config] 查看帮助
+        config 查看配置说明
 DOC;
-    echo PHP_EOL;
+        echo PHP_EOL;
+    }
+
     exit;
 }
 
 // 远程配置
 if ($isGlobal) {
-    setGlobal($action);
+    if ($action == 'config') {
+        echo getenv('HOME') . '/.websyncrc.php' . PHP_EOL;
+    } else {
+        setGlobal($action);
+    }
     exit;
 }
 
@@ -177,38 +183,35 @@ function initSetting(string $pathConf)
         exit;
     }
 
-    $sampleConf = file_get_contents(__DIR__ . '/.websyncrc.example.php');
+    // 远程服务器名
+    $hostname = readline("远程服务器名: [hostname1] \n");
 
-    $readHostname = readline("远程服务器名: [hostname1] \n");
-    $sampleConf = str_replace('hostname1', $readHostname ?: 'hostname1', $sampleConf);
-
-    // 读取 全局配置-远程服务器 如果没有全局配置则自动新建
+    // 全局配置: 读取 全局配置-远程服务器 如果没有全局配置则自动新建
     $pathGlobal = getenv('HOME') . '/.websyncrc.php';
     if (is_file($pathGlobal)) {
         $globalConf = include($pathGlobal);
-        if (!isset($globalConf['remotes'][$readHostname])) {
-            setGlobal('remote', ['hostname' => $readHostname]);
+        if (!isset($globalConf['remotes'][$hostname])) {
+            setGlobal('remote', ['hostname' => $hostname]);
         }
     } else {
-        setGlobal('remote', ['hostname' => $readHostname]);
+        setGlobal('remote', ['hostname' => $hostname]);
     }
 
-    // 重新加载全局配置文件
-    $globalConf = include($pathGlobal);
-    $globalHost = $globalConf['remotes'][$readHostname];
+    // 本项目的配置
+    $sampleConf = include(__DIR__ . '/.websyncrc.example.php');
+    $sampleConf['remotes'] = [];
+    if ($hostname) {
+        $sampleConf['pull'] = $hostname;
+        $sampleConf['push'] = [$hostname];
+    }
 
-    $sampleConf = str_replace("websync@host", $globalHost['@'], $sampleConf);
-    $sampleConf = str_replace('ssh -p 22', $globalHost['command'], $sampleConf);
-    $sampleConf = str_replace('/path/to/', $globalHost['path'], $sampleConf);
-    $sampleConf = str_replace('websync:websync', $globalHost['chown'], $sampleConf);
-
-    echo $sampleConf . PHP_EOL;
-    if (strtolower(readline("请确认 [Y/n] \n") ?: 'Y') != 'y') {
+    echo var_export($sampleConf, true) . PHP_EOL;
+    if (strtolower(readline("请确认本项目的配置 [Y/n] \n") ?: 'Y') != 'y') {
         exit;
     }
 
-    file_put_contents($pathConf, $sampleConf);
-    echo "{$pathConf} 配置文件初始化完成，可随时修改" . PHP_EOL;
+    file_put_contents($pathConf, "<?php\n\nreturn " . var_export($sampleConf, true) . ';' . PHP_EOL);
+    echo "{$pathConf} 初始化完成，可随时修改配置文件" . PHP_EOL;
 }
 
 /**
@@ -239,7 +242,7 @@ function setGlobal(string $action, $opt = [])
             $command = $globalHost['command'] ?? 'ssh -p 22';
             $globalHost['command'] = readline("远程命令: [{$command}] \n") ?: $command;
 
-            $path = $globalHost['path'] ?? '/path/to/';
+            $path = $globalHost['path'] ?? '/path/to';
             $globalHost['path'] = readline("远程根目录: [{$path}] \n") ?: $path;
 
             $chown = $globalHost['chown'] ?? 'websync:websync';
@@ -247,12 +250,12 @@ function setGlobal(string $action, $opt = [])
 
             $globalConf['remotes'][$readHostname] = $globalHost;
             echo var_export($globalConf['remotes'], true) . PHP_EOL;
-            if (strtolower(readline("请确认 [Y/n] \n") ?: 'Y') != 'y') {
+            if (strtolower(readline("请确认全局远程服务器配置 [Y/n] \n") ?: 'Y') != 'y') {
                 exit;
             }
 
-            file_put_contents($pathGlobal, "<?php\n    return " . var_export($globalConf, true) . ';' . PHP_EOL);
-            echo "{$pathGlobal} 全局配置设置完成，可随时修改" . PHP_EOL;
+            file_put_contents($pathGlobal, "<?php\n\nreturn " . var_export($globalConf, true) . ';' . PHP_EOL);
+            echo "{$pathGlobal} 设置完成，可随时修改配置文件" . PHP_EOL;
 
             break;
         default:
@@ -268,7 +271,9 @@ function setGlobal(string $action, $opt = [])
  */
 function checkVersion(array $conf)
 {
-    if (empty($conf['version']) || !version_compare(VERSION, $conf['version'][0], $conf['version'][1])) {
+    if (empty($conf['version'])
+        || !is_string($conf['version'])
+        || !version_compare(VERSION, $conf['version'], '>=')) {
         echo '配置文件版本过低，请先备份好再重新生成配置文件' . PHP_EOL;
         return false;
     }
