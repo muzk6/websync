929fb4faa6bf224c0b24d760327627f88224e5ac
yangjunping <yangjunping@115.com>
2019-12-18 09:44:04 +0800

    fix. websync.phar 覆盖生成后不正常

--- build.php
+++ build.php
@@ -1,16 +1,17 @@
 <?php
 
-$outfile = 'websync.phar'; // 成品文件名
-$extList = ['php']; // 需要打包的文件后缀
+$outfile = 'websync.phar';
+$extList = ['php'];
+
+unlink(__DIR__ . '/' . $outfile);
 $phar = new Phar(__DIR__ . '/' . $outfile, FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, $outfile);
 
 $phar->startBuffering();
 
-// 将后缀名相关的文件打包
 foreach ($extList as $ext) {
     $phar->buildFromDirectory(__DIR__, '/\.' . $ext . '$/');
 }
-$phar->delete('build.php'); // 排除 build.php 本身
+$phar->delete('build.php');
 $phar->setStub("#! /usr/bin/env php\n" . $phar->createDefaultStub('websync.php')); // 程序入口
 
 $phar->stopBuffering();
